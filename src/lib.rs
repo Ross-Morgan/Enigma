f37cb0c82bd96b3f44f2cc89232d650d62a1e4ea
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-25 01:21:02 +0100

    add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ pub mod machine;
 pub mod plugboard;
 pub mod reflector;
 pub mod rotors;
+
+
+pub mod prelude {
+    use super::*;
+
+    pub use machine::EnigmaMachine;
+    pub use plugboard::Plugboard;
+    pub use reflector::Reflector;
+
+    pub use rotors::{Rotor, load_rotor_preset};
+    pub use rotors::presets::groups as rotor_presets;
+}
