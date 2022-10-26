91ff6df46e8c2012aff357138a3c8a70eaea14e6
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-26 01:43:29 +0100

    add machine preset enum

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod plugboard;
 pub mod reflector;
 pub mod rotors;
 pub mod cycle_notation;
+pub mod types;
 
 pub mod prelude {
     use super::*;
@@ -14,7 +15,7 @@ pub mod prelude {
     pub use reflector::{Reflector, load_reflector_preset};
     pub use reflector::presets as reflector_presets;
 
-    pub use rotors::{Rotor, load_rotor_preset};
+    pub use rotors::{Rotor, RotorStack};
     pub use rotors::presets::groups as rotor_presets;
 
 }
