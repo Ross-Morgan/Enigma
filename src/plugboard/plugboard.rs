54342952c2feb40355993ca052e3d1ef21b2b88b
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-27 00:15:15 +0100

    change Vec in Plugboard for HashMap

--- src/plugboard/plugboard.rs
+++ src/plugboard/plugboard.rs
@@ -1,17 +1,25 @@
+use std::collections::HashMap;
+
+use crate::cycle_notation::CycleNotation;
+
 #[derive(Clone, Debug)]
 pub struct Plugboard {
-    pub(in super) mappings: Vec<char>
+    pub(in super) mappings: HashMap<char, char>
 }
 
 
 impl Plugboard {
     pub fn new() -> Self {
-        Self { mappings: vec![] }
+        Self { mappings: HashMap::new() }
+    }
+
+    pub fn from_cycle_notation(notation: CycleNotation) -> Self {
+        Self { mappings: notation.mappings }
     }
 }
 
 impl Plugboard {
     pub fn plug(&self, c: char) -> char {
-        c
+        *self.mappings.get(&c).expect("Character not in plugboard mappings")
     }
 }
