46b17a6ae65b3193a3cecedb0ead31ad8911bd58
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-26 02:17:45 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ fn main() {
     let reflector = Reflector::new();
     let rotors = RotorStack::from_preset(rotor_presets::TECHNICAL);
 
-    let machine: EnigmaMachine<10> = EnigmaMachine::<10>::build_with()
+    let mut machine: EnigmaMachine<10> = EnigmaMachine::<10>::build_with()
         .plugboard(plugboard)
         .reflector(reflector)
         .rotors(rotors)
         .build();
 
-    println!("{machine:#?}")
+    println!("{machine:#?}");
+
+    let s = machine.encrypt("How are you?");
+
+    println!("Encrypted: {s:?}");
 }
