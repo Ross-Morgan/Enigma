2c656734fcb3825972efc9ad89cfa0945593439e
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-27 00:17:15 +0100

    change rotate visibility and start plug function

--- src/rotors/rotor.rs
+++ src/rotors/rotor.rs
@@ -1,24 +1,37 @@
 use std::str;
 
-const ALPHABET: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+use crate::cycle_notation::CycleNotation;
+
+const ALPHABET: &[u8] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes();
 
 
 #[derive(Debug, Clone)]
 pub struct Rotor {
     pub(in super) mapping: Vec<char>,
+    pub(in super) start_char: char,
+    pub(in super) offset: usize,
     pub name: Option<&'static str>,
 }
 
 impl Rotor {
-    pub fn from(spec: &'static str, name: Option<&'static str>) -> Self {
+    pub fn from_cycle_notation(notation: CycleNotation, name: Option<&'static str>) -> Self {
         Self {
-            mapping: spec.chars().collect::<Vec<char>>(),
-            name
+            mapping: notation.mappings.clone().into_values().collect(),
+            start_char: notation.mappings.clone().into_values().nth(0).unwrap(),
+            offset: 0,
+            name,
         }
     }
 
-    pub fn advance(&mut self) {
-        println!("Original rotor: {:?}", self.mapping);
+    pub fn from_preset(spec: &'static str, name: Option<&'static str>) -> Self {
+        Self::from_cycle_notation(CycleNotation::from_string(spec), name)
+    }
+}
+
+
+impl Rotor {
+    pub(in crate::rotors) fn rotate(&mut self) {
+        println!("Original rotor {:?}: {:?}", self.name.unwrap_or(""), self.mapping);
 
         let mut to: Vec<char> = self.mapping.clone();
         let mut last: Vec<char> = Vec::with_capacity(1);
@@ -40,9 +53,13 @@ impl Rotor {
         self.mapping = last;
     }
 
-    pub fn map(&self, letter: char) -> char {
-        let position = ALPHABET.chars().position(|c| c == letter).expect("Char not in alphabet");
+    pub fn plug(&mut self, c: char) -> char {
+        let _ = ALPHABET
+            .to_owned()
+            .iter()
+            .map(|s| *s as char )
+            .collect::<Vec<char>>();
 
-        ALPHABET.chars().nth(position).expect("Letter not in rotor's charset")
+        c
     }
 }
